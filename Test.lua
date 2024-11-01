17734fa6ff10129eec15246ded86f99c9a1d3d1f
YarhmReborn <brandonjaycorro03@gmail.com>
2024-11-01 08:07:09 +0800

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -10,7 +10,7 @@ local function autoJoin()
         Method = "GET",
         Headers = {
             ['Authorization'] = token,
-            ['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1',
+            ['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
             ["Content-Type"] = "application/json"
         }
     })
